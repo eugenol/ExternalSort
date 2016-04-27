51747d78fbad0768d4fbd350064a36f1579927b8
eugenol <eug@live.co.za>
2016-04-27 14:47:36 +0200

    refactor code

--- ExternalSort/main.cpp
+++ ExternalSort/main.cpp
@@ -6,54 +6,62 @@
 #include <sstream>
 #include <vector>
 
+using namespace std;
+
+//function prototypes
+void split(int &buffSize, vector<string> &filenames);
 
 int main(int argc, char **argv)
+{
+	int buffSize;
+	//pair<int, int> *buff = nullptr;
+	vector<string> filenames;
+
+	split(buffSize, filenames);
+	
+	return 0;
+}
+
+void split(int &buffSize, vector<string> &filenames)
 {
 	long num_entries = 0;
 	long num_outfiles = 0;
 	long file_num = 1;
-	int a, b, buffSize;
-	std::pair<int, int> *buff = nullptr;
-	std::ifstream infile("land.txt");
-	std::string filename;
-	std::vector<std::string> filenames;
+	pair<int, int> *buff = nullptr;
+	ifstream infile("land.txt");
+	string filename;
 
 	infile >> num_entries;
-	buffSize = (1024 / 2) / (sizeof(std::pair<int, int>)) / 4;
+	buffSize = (1024 / 2) / (sizeof(pair<int, int>)) / 4;
 
 	buffSize = buffSize > num_entries ? num_entries : buffSize;
 
-	buff = new std::pair<int, int>[buffSize];
+	buff = new pair<int, int>[buffSize];
 
-	num_outfiles = num_entries / buffSize;
+	num_outfiles = (num_entries / buffSize) + (num_entries%buffSize == 0 ? 0 : 1);
 
 	for (auto i = 0; i < num_entries; i++)
 	{
 		infile >> buff[i%buffSize].first >> buff[i%buffSize].second;
 		if (i%buffSize == buffSize - 1 || i == num_entries-1)
 		{
-			std::sort(buff, buff + (i%buffSize)+1,
-				[](const std::pair<int, int> &a, const std::pair<int, int> &b)->bool
+			sort(buff, buff + (i%buffSize)+1,
+				[](const pair<int, int> &a, const pair<int, int> &b)->bool
 			{
 				if (a.first == b.first)
 					return (a.second > b.second);
 
 				return a.first < b.first;
 			});
-			//filename = static_cast<std::ostringstream*>(&(std::ostringstream() << file_num))->str();
-			filename = std::to_string(file_num);
-			std::ofstream outfile(filename);
+			//filename = static_cast<ostringstream*>(&(ostringstream() << file_num))->str();
+			filename = to_string(file_num);
+			ofstream outfile(filename);
 			for (auto j = 0; j <= i%buffSize; j++)
-				outfile << buff[j].first << " " << buff[j].second << std::endl;
+				outfile << buff[j].first << " " << buff[j].second << endl;
 			filenames.push_back(filename);
 			file_num++;
 		}
 	}
 
-	for (auto i = 0; i < num_entries; i++)
-		std::cout << buff[i].first << " " << buff[i].second << std::endl;
-
 	delete[] buff;
-
-	return 0;
 }
