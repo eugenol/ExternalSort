53c3a54248bba5ca996071255f572eb79aa84f89
eugenol <eug@live.co.za>
2016-04-28 12:35:48 +0200

    fixes

--- ExternalSort/main.cpp
+++ ExternalSort/main.cpp
@@ -4,29 +4,37 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
-#include <vector>
+#include <deque>
 
 using namespace std;
 
 //function prototypes
-void split(int &buffSize, vector<string> &filenames);
+int split(int &buffSize, deque<string> &filenames);
+//void merge(int const &buffSize, deque<string> &filenames);
+void merge(int const &buffSize, deque<string> &filenames, int numFiles);
+int fillBuff(ifstream &file, pair<int, int> *buff, int const &buffSize);
+void writeBuff(ofstream &file, pair<int, int> *buff, int buffSize);
+bool compare(const pair<int, int> &a, const pair<int, int> &b);
 
 int main(int argc, char **argv)
 {
 	int buffSize;
+	int numFiles;
 	//pair<int, int> *buff = nullptr;
-	vector<string> filenames;
+	deque<string> filenames;
 
-	split(buffSize, filenames);
+	numFiles = split(buffSize, filenames);
 	
+	merge(buffSize, filenames, numFiles);
+
 	return 0;
 }
 
-void split(int &buffSize, vector<string> &filenames)
+int split(int &buffSize, deque<string> &filenames)
 {
 	long num_entries = 0;
 	long num_outfiles = 0;
-	long file_num = 1;
+	long file_num = 0;
 	pair<int, int> *buff = nullptr;
 	ifstream infile("land.txt");
 	string filename;
@@ -64,4 +72,201 @@ void split(int &buffSize, vector<string> &filenames)
 	}
 
 	delete[] buff;
+
+	return file_num;
+}
+
+void merge(int const &buffSize, deque<string> &filenames, int numFiles)
+{
+	ifstream infile1, infile2;
+	ofstream outfile;
+	pair<int, int> *inBuff1 = nullptr, *inBuff2 = nullptr, *outBuff = nullptr;
+	int i = 0, j = 0, k = 0, count1, count2, level = 0;
+	string filename;
+
+	inBuff1 = new pair<int, int>[buffSize];
+	inBuff2 = new pair<int, int>[buffSize];
+	outBuff = new pair<int, int>[2 * buffSize];
+	
+	while (numFiles)
+	{
+		if (numFiles == 0)
+			break;
+		else if (numFiles == 1)
+		{
+			rename(filenames[0].c_str(), "output.txt");
+			filenames.pop_front();
+			numFiles--;
+		}
+		else
+		{
+			int numPairs = numFiles / 2;
+			level++;
+			int iter;
+			for (iter = 0; iter < numPairs; iter++)
+			{
+				filename = to_string(level).append("-").append(to_string(iter));
+				outfile.open(filename.c_str());
+				infile1.open(filenames[0].c_str());
+				infile2.open(filenames[1].c_str());
+				count1 = fillBuff(infile1, inBuff1, buffSize);
+				count2 = fillBuff(infile2, inBuff2, buffSize);
+				i = j = k = 0;
+
+				while (j < count1 ||  k < count2)
+				{
+					if (j < count1 && k < count2)
+					{
+						if (compare(inBuff1[j], inBuff2[k]))
+							outBuff[i++] = inBuff1[j++];
+						else
+							outBuff[i++] = inBuff2[k++];
+					}
+					else
+					{
+						if (j < count1)
+							outBuff[i++] = inBuff1[j++];
+						else if (k < count2)
+							outBuff[i++] = inBuff2[k++];
+					}
+
+					if (i == 2 * buffSize)
+					{
+						writeBuff(outfile, outBuff, 2 * buffSize);
+						i = 0;
+					}
+
+					if (j == count1 && count1!=0)
+					{
+						count1 = fillBuff(infile1, inBuff1, buffSize);
+						j = 0;
+					}
+
+					if (k == count2 && count2!=0)
+					{
+						count2 = fillBuff(infile2, inBuff2, buffSize);
+						k = 0;
+					}
+
+				}
+
+				if(i)
+					writeBuff(outfile, outBuff, i);
+
+				infile1.close();
+				infile2.close();
+				outfile.close();
+				filenames.push_back(filename);
+				remove(filenames[0].c_str());
+				remove(filenames[1].c_str());
+				filenames.pop_front();
+				filenames.pop_front();
+
+			}
+
+			if (numFiles == 2 * numPairs)
+			{
+				numFiles = numPairs;
+			}
+			else
+			{
+				numFiles = numPairs + 1;
+				//iter++;
+				filename = to_string(level).append("-").append(to_string(iter));
+				rename(filenames[0].c_str(), filename.c_str());
+				filenames.push_back(filename);
+				filenames.pop_front();
+			}
+		}
+	}
+}
+/*
+void merge(int const &buffSize, deque<string> &filenames)
+{
+	//repetedly merge the files
+	ifstream infile1, infile2;
+	ofstream outfile("output.txt");
+	pair<int, int> *inBuff1 = nullptr, *inBuff2 = nullptr, *outBuff = nullptr;
+	bool file1, file2;
+	int i=0, j=0, k=0, count1, count2;
+
+	inBuff1 = new pair<int, int>[buffSize];
+	inBuff2 = new pair<int, int>[buffSize];
+	outBuff = new pair<int, int>[2*buffSize];
+
+	while (!filenames.empty())
+	{
+		infile1.open(filenames[0]);
+		filenames.pop_front();
+		file1 = true;
+		infile2.open(filenames[0]);
+		filenames.pop_front();
+		file2 = true;
+
+		count1 = fillBuff(infile1, inBuff1, buffSize);
+		count2 = fillBuff(infile2, inBuff2, buffSize);
+
+		while (file1 || file2)
+		{
+			if (file1 && file2 && j < count1 && k < count2)
+			{
+				if(compare(inBuff1[j],inBuff2[k]))
+					outBuff[i++] = inBuff1[j++];
+				else
+					outBuff[i++] = inBuff2[k++];
+			}
+			else
+			{
+				if(file1 && j < count1)
+					outBuff[i++] = inBuff1[j++];
+				else if(file2 && k < count2)
+					outBuff[i++] = inBuff2[k++];
+			}
+
+			if (i == 2 * buffSize - 1)
+				writeBuff(outfile,outBuff,2*buffSize);
+		}
+	}
+}
+*/
+/*
+int fillBuff(ifstream &file, pair<int, int> *buff, int const &buffSize)
+{
+	int count = 0;
+	while (count < buffSize && ~file.eofbit)
+	{
+		file >> buff[count].first >> buff[count].second;
+		count++;
+	}
+	return count;
+}
+*/
+
+int fillBuff(ifstream &file, pair<int, int> *buff, int const &buffSize)
+{
+	int count = 0;
+	string temp;
+	while (count < buffSize && getline(file,temp))
+	{
+		stringstream tempstream(temp);
+		tempstream >> buff[count].first >> buff[count].second;
+		count++;
+	}
+	return count;
+}
+
+void writeBuff(ofstream &file, pair<int, int> *buff, int buffSize)
+{
+	for (int i = 0; i < buffSize; i++)
+	{
+		file << buff[i].first << " " << buff[i].second << endl;
+	}
+}
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b)
+{
+	if (a.first == b.first)
+		return (a.second > b.second);
+
+	return a.first < b.first;
 }
